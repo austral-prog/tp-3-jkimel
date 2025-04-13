bbd53e955522aac5d8f4e46ad0aab5c2d41548e2
jkimel <jkimel@mail.austral.edu.ar>
2025-04-13 19:11:40 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,5 +1,4 @@
 def slice_advanced():
-    # Código a implementar utilizando input.
-
-# Para verificar este ejercicio ejecutar el comando
-# `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
+    char = input('Ingrese los caracteres que desee: ')
+    print(char[4::2])
+slice_advanced()
