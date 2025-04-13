451b9062ed7b20fedb132677012cf4170b22772d
jkimel <jkimel@mail.austral.edu.ar>
2025-04-13 19:14:13 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -2,6 +2,5 @@ def check_vowels():
     name = input("Ingrese un nombre: ")
     name_lower = name.lower()
     vowels = ['a', 'e', 'i', 'o', 'u']
-    
     for vowel in vowels:
         print(f"Contiene {vowel}: {vowel in name_lower}")
